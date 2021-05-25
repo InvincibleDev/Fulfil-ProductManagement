42a368cfb295ab44fc4f8c3f4119602202c7a552
Ganesh  Prasad <ganesh.prasad@nrutech.in>
2021-05-25 17:57:35 +0530

    Updated Base.py database settings

--- ProductManagement/ProductManagement/settings/base.py
+++ ProductManagement/ProductManagement/settings/base.py
@@ -65,9 +65,6 @@ REST_FRAMEWORK = {
 DATABASES = {
       'default': {
           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-          'OPTIONS' : {
-                'options': '-c search_path=django,public'
-            },
           'NAME': os.environ.get('DB_NAME'),
           'USER': os.environ.get('DB_USER'),
           'PASSWORD': os.environ.get('DB_PASSWORD'),
