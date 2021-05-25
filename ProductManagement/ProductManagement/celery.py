53f3c25cf11558fab4e2db68dd2f9337e142163a
Ganesh  Prasad <ganesh.prasad@nrutech.in>
2021-05-25 17:55:57 +0530

    Updated requirements.txt

--- ProductManagement/ProductManagement/celery.py
+++ ProductManagement/ProductManagement/celery.py
@@ -12,7 +12,3 @@ app = Celery('product-management')
 app.config_from_object('django.conf:settings')
 
 app.autodiscover_tasks(settings.INSTALLED_APPS)
-
-@app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
